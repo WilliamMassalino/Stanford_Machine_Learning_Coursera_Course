c6d7a8a4fb0e68029a49d86dba01bc7577237ef0
WILLIAM MASSALINO <41436246+WilliamMassalino@users.noreply.github.com>
2019-07-11 11:52:35 -0300

    Add files via upload

---
+++ machine-learning-ex2/ex2/Regularized_Logistic_Regression.m
@@ -0,0 +1,70 @@
+data = load('ex2data2.txt');
+X = data(:,[1, 2]); y =data(:,3);
+plotData(X, y);
+% Put some labels 
+hold on;
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+% Specified in plot order
+legend('y = 1', 'y = 0')
+hold off;
+
+% Add Polynomial Features
+% Note that mapFeature also adds a column of ones for us, so the intercept term is handled
+X = mapFeature(X(:,1), X(:,2));
+
+% Cost function and Gradient:
+
+% Initialize fitting parameters 
+
+initial_theta = zeros(size(X,2),1);
+
+% Set regularization parameter lambda to 1
+
+lambda = 0;
+
+% Compute and display initial cost and gradient for regularized logistic regression
+[cost, grad] = costFunctionReg(initial_theta, X, y,lambda);
+
+fprintf('Cost initial theta (zeros) %f\n', cost);
+
+% Learning parameters using fminunc
+
+
+% Initialize fitting parameters
+initial_theta = zeros(size(X, 2), 1);
+
+lambda = 0;
+% Set Options
+options = optimoptions(@fminunc,'Algorithm','Quasi-Newton','GradObj', 'on', 'MaxIter', 1000);
+
+% Optimize
+[theta, J, exit_flag] = fminunc(@(t)(costFunctionReg(t, X, y, lambda)), initial_theta, options);
+% Plot Boundary
+plotDecisionBoundary(theta, X, y);
+hold on;
+title(sprintf('lambda = %g', lambda))
+
+% Labels and Legend
+xlabel('Microchip Test 1')
+ylabel('Microchip Test 2')
+
+legend('y = 1', 'y = 0', 'Decision boundary')
+hold off;
+
+% Compute accuracy on our training set
+p = predict(theta, X);
+
+fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
+
+
+
+
+
+
+
+
+
+
+
